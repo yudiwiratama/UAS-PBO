e3c73533995ed439ad569db058771bcc1fa830d5
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2025-02-18 04:07:34 +0700

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE spp_payment;
+USE spp_payment;
+
+CREATE TABLE pembayaran (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_siswa INT NOT NULL,
+    nama_siswa VARCHAR(100) NOT NULL,
+    kelas VARCHAR(50) NOT NULL,
+    jurusan VARCHAR(50) NOT NULL,
+    metode_pembayaran VARCHAR(50) NOT NULL,
+    jumlah DECIMAL(10, 2) NOT NULL
+);
+
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '1223';
+flush privileges;
