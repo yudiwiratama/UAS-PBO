3e9aac66ff607b29e1d7d30d3093b4cf1ae2124c
yudiwiratama <97220605+yudiwiratama@users.noreply.github.com>
2025-02-21 06:39:06 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,6 +1,20 @@
+-- buat database
 CREATE DATABASE spp_payment;
 USE spp_payment;
 
+-- buat table untuk data autentikasi login
+CREATE TABLE admin (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL
+);
+
+-- Contoh data admin
+INSERT INTO admin (username, password) VALUES ('admin', 'admin123');
+
+
+
+-- buat table untuk memuat data
 CREATE TABLE pembayaran (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_siswa INT NOT NULL,
@@ -8,5 +22,7 @@ CREATE TABLE pembayaran (
     kelas VARCHAR(50) NOT NULL,
     jurusan VARCHAR(50) NOT NULL,
     metode_pembayaran VARCHAR(50) NOT NULL,
-    jumlah DECIMAL(10, 2) NOT NULL
+    jumlah DECIMAL(10, 2) NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
+
